22bc33b23689dde36a05b653d43dd1604ddce8d3
Nelson Foltz <nelsfoltz@gmail.com>
2021-07-05 14:55:53 -0400

    406: adding a data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [new Todo('learn react'), new Todo('learn typescript')];
   return (
     <div>
-      <Todos items={['learn react', 'learn typescript']} />
+      <Todos items={todos} />
     </div>
   );
 }
