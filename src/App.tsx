c561a324ae80c7e30f5f17cc5a49778ea0e838a9
Nelson Foltz <nelsfoltz@gmail.com>
2021-07-06 08:38:27 -0400

    412: adding styling and of course it's broken

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
+import './App.css';
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
